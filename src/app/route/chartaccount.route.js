15b01cac62ffd9199d1c863eb878764b9dfffd75
Waldir Borba Junior <wborbajr@gmail.com>
2020-05-08 18:33:26 -0300

    feat: Chart Account - CRUD - done

---
+++ src/app/route/chartaccount.route.js
@@ -0,0 +1,19 @@
+module.exports = function (app) {
+
+    const chartaccounts = require('../controller/chartaccount.controller.js');
+
+    // Create a new 
+    app.post('/api/chartaccounts/create', chartaccounts.create);
+
+    // Retrieve all 
+    app.get('/api/chartaccounts', chartaccounts.findAll);
+
+    // Retrieve a single by Id
+    app.get('/api/chartaccounts/:chartaccountId', chartaccounts.findByPk);
+
+    // Update a with Id
+    app.put('/api/chartaccounts/:chartaccountId', chartaccounts.update);
+
+    // Delete a with Id
+    app.delete('/api/chartaccounts/:chartaccountId', chartaccounts.delete);
+}
--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
 const db = require('./app/config/db.config.js');
-  
+
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   console.log('Drop and Resync with { force: true }');
 });
 
@@ -19,12 +19,13 @@ require('./app/route/coverage.route.js')(app);
 require('./app/route/licensecountry.route.js')(app);
 require('./app/route/classcustomer.route.js')(app);
 require('./app/route/cashflow.route.js')(app);
+require('./app/route/chartaccount.route.js')(app);
 
 // Create a Server
 var server = app.listen(9090, function () {
- 
+
   var host = server.address().address
   var port = server.address().port
- 
+
   console.log("App listening at http://%s:%s", host, port)
 })
